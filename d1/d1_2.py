8ab0ce0347c131d9eff04a784deb58b7b607036b
SartajGulati <Sartajsinghgulati@gmail.com>
2025-01-04 21:39:45 -0500

    d1

--- d1/d1_2.py
+++ d1/d1_2.py
@@ -2,20 +2,23 @@ import csv
 
 
 l = [] 
-r = []
+r = {}
 
 with open("input.txt") as file: 
     reader = csv.reader(file, delimiter=' ')
     for row in reader:
         l.append(row[0])
-        r.append(row[3])
+        if row[3] not in r:
+            r[row[3]] = 1
+        else:
+            r[row[3]] += 1
 
 l.sort()
-r.sort()
+
 
 sum = 0
 
 for i in range(len(l)):
-    sum += abs(int(r[i]) - int(l[i]))
+    sum += 0 if l[i] not in r else int(l[i]) * r[l[i]]
 
 print(sum)
